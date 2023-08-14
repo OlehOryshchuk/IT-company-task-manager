667a62177412313cee1ca0b6c0449c47ce0e9527
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-08-14 13:07:49 +0300

    feat: Create models Position,Worker,Team in team_manager/models

--- team_manager/models.py
+++ team_manager/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.CASCADE,
+        related_name="workers",
+        null=True
+    )
+
+    class Meta:
+        verbose_name = "worker"
+        verbose_name_plural = "workers"
+
+    def __str__(self):
+        return self.username
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True, blank=True)
+    members = models.ManyToManyField(
+        Worker,
+        related_name="workers",
+    )
+
+    class Meta:
+        verbose_name = "team",
+        verbose_name_plural = "teams"
+
+    def __str__(self):
+        return self.name
