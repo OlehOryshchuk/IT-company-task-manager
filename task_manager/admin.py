8d774460d8d6841ff426ba45ac80dd1d9516ebe9
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-08-14 19:28:04 +0300

    feat: Register TaskType,Task,Project models

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import TaskType, Task, Project
+
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ["name"]
+    list_filter = ["name"]
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+    list_filter = ["tags"]
+    list_display = [
+        "name",
+        "description",
+        "deadline",
+        "is_completed",
+        "priority"
+    ]
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "is_completed",
+        "priority",
+        "deadline",
+    ]
+    search_fields = ["name"]
+    list_filter = ["name"]
