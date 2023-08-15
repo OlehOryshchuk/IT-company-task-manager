5c5e36254bfcb3bd0d85ac0c0dde25c84029eec4
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-08-15 15:01:32 +0300

    feat: Add static files serving for development

--- it_company_task_manager/urls.py
+++ it_company_task_manager/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('taggit_autosuggest/', include('taggit_autosuggest.urls')),
     path("accounts/", include("django.contrib.auth.urls"))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
