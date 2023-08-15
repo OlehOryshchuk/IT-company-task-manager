c2859dcbde76e56091dd92e60e9933f74d3235cd
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-08-15 21:58:52 +0300

    feat: Create List, Detail,Update,Delete,Create views for model Worker

--- team_manager/views.py
+++ team_manager/views.py
@@ -33,3 +33,62 @@ def index(request):
 
     return render(request, "index.html", context=context)
 
+
+class WorkerListView(LoginRequiredMixin, generic.ListView):
+    model = Worker
+    paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        username = self.request.GET.get("username", "")
+        search_form = WorkerSearchForm(initial={
+            "username": username
+        })
+
+        context["search_form"] = search_form
+
+        return context
+
+    def get_queryset(self) -> QuerySet:
+        queryset = Worker.objects.all().select_related("position")
+        username = self.request.GET.get("username")
+
+        if username:
+            return queryset.filter(username__icontains=username)
+        return queryset
+
+
+class WorkerDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Worker
+    queryset = Worker.objects.prefetch_related(
+        "tasks__task_type", "tasks__tags", 'teams'
+    )
+
+
+class WorkerCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Worker
+    form_class = WorkerCreationForm
+    success_url = reverse_lazy("team_manager:worker-list")
+
+
+class WorkerUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Worker
+    fields = [
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "position",
+    ]
+
+    def get_success_url(self):
+        return reverse_lazy("team_manager:worker-detail", kwargs={"pk": self.object.pk})
+
+
+class WorkerDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Worker
+    success_url = reverse_lazy("team_manager:worker-list")
+
+
+
