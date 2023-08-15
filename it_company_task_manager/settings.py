f0e65dc14c3f57db02163b4d11a569c556c09e08
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-08-15 15:47:15 +0300

    feat: Add stattic and templates configurations

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = "it_company_task_manager.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -127,6 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
