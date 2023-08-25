2ce0f05799e12a48c6c2f166d0af42ca63fc7347
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-08-25 19:34:01 +0300

    feat: Create 'valid_deadline'&'task_filter_view' for checking deadlines and filtering task

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+import datetime
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
+from django.db.models import QuerySet
 
-# Create your views here.
+from .models import TaskType, Task, Project
+from .form import (
+    TaskFilterForm,
+    TaskSearchForm,
+    TaskCreateForm,
+    TaskChangeStatusForm,
+    ProjectSearchForm,
+    ProjectCreateForm,
+)
+
+
+def valid_deadline(deadline: datetime) -> bool:
+    if deadline < datetime.date.today():
+        return True
+    return False
+
+
+def task_filter_view(request):
+    if request.GET.get("reset"):
+        request.session.pop("task_filter", None)  # Remove the session filter data
+
+    context = {
+        "task_filter": TaskFilterForm(initial=request.session.get("task_filter", {}))
+    }
+
+    return render(request, "task_manager/task_filter.html", context=context)
