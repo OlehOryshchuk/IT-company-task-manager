af752b20d80e15703543e845717bee8053d50906
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-08-26 17:45:38 +0300

    feat: Create PositionFormTest class for testing Position's forms

--- team_manager/tests/test_forms.py
+++ team_manager/tests/test_forms.py
@@ -10,3 +10,21 @@ from ..form import (
     TeamCreationForm,
     TeamJoinRemoveForm,
 )
+
+
+class PositionFormTest(TestCase):
+
+    def setUp(self) -> None:
+        self.form = PositionSearchForm()
+
+    def test_search_form_field_and_label(self):
+        self.assertTrue(self.form.fields.get("name"))
+        self.assertTrue(self.form.fields["name"].label == "")
+
+    def test_search_form_placeholder(self):
+        rendered_html = self.form.as_p()
+
+        self.assertIn('placeholder="Search by position name"', rendered_html)
+
+    def test_search_form_is_not_required(self):
+        self.assertFalse(self.form.fields["name"].required)
