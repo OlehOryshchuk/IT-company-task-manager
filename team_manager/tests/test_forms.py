3fb2f80b29bdc644ea733cdf7103fe45f3e4098a
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-08-26 17:25:32 +0300

    feat: Create 'test_forms.py' for testing forms in team_manager/form.py

---
+++ team_manager/tests/test_forms.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+from ..form import (
+    PositionSearchForm,
+
+    WorkerCreationForm,
+    WorkerSearchForm,
+
+    TeamSearchForm,
+    TeamCreationForm,
+    TeamJoinRemoveForm,
+)
