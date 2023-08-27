a691151d6eccc32eb2a3dcbd248c3c2df437d216
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-08-27 17:55:53 +0300

    test: Create 'test_admins.py' for testing models on admin site

---
+++ task_manager/tests/test_admins.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from ..models import TaskType, Task, Project
+
+
+class AdminSiteTest(TestCase):
+    pass
