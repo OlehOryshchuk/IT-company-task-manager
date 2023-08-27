67db08a46321ee493698cb81abe9868a20a2d037
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-08-27 21:53:04 +0300

    test: Test TaskFilterForm, test fields 'task_type'&'is_completed' is not required and test empty label

--- task_manager/tests/test_forms.py
+++ task_manager/tests/test_forms.py
@@ -9,6 +9,19 @@ from ..form import (
 
     ProjectSearchForm,
     ProjectCreateForm,
+
+    valid_deadline
 )
+from ..models import TaskType
+
+
+class TaskFormTest(TestCase):
+    def setUp(self) -> None:
+        self.filter_form = TaskFilterForm()
+
+    def test_filter_form_empty_label_and_required_should_be_false(self):
+        self.assertEqual(self.filter_form.fields["task_type"].empty_label, "Filter by task type")
+        self.assertTrue(self.filter_form.fields["task_type"].required is False)
+        self.assertTrue(self.filter_form.fields["is_completed"].required is False)
 
 
