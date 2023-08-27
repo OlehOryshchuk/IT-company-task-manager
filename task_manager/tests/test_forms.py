8d911f3ebd5c10cbe16473ea265a18e3c17885ef
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-08-27 22:11:39 +0300

    test: Test TaskCreateForm, test valid and invalid creations of task

--- task_manager/tests/test_forms.py
+++ task_manager/tests/test_forms.py
@@ -18,6 +18,9 @@ from ..models import TaskType
 class TaskFormTest(TestCase):
     def setUp(self) -> None:
         self.filter_form = TaskFilterForm()
+        self.user = get_user_model().objects.create(
+            username="testuser", password="test123"
+        )
 
     def test_filter_form_empty_label_and_required_should_be_false(self):
         self.assertEqual(self.filter_form.fields["task_type"].empty_label, "Filter by task type")
@@ -42,3 +45,27 @@ class TaskFormTest(TestCase):
         self.assertEqual(search_form.fields["name"].required, False)
         self.assertEqual(search_form.fields["name"].label, "")
         self.assertIn('placeholder="Search by task name"', rendered_html)
+
+    def test_form_valid_data(self):
+        task_type = TaskType.objects.create(name="bug")
+        form_data = {
+            "name": "Test Task",
+            "description": "This is a test task",
+            "deadline": "2023-08-31",
+            "priority": "high",
+            "task_type": task_type.id,
+            "owner": self.user.id,
+            "is_completed": True,
+        }
+
+        form = TaskCreateForm(data=form_data, user=self.user)
+        self.assertTrue(form.is_valid())
+
+    def test_form_invalid_data(self):
+        form_data = {
+            "name": "Test Task",
+            "priority": "high",
+        }
+
+        form = TaskCreateForm(data=form_data, user=self.user)
+        self.assertFalse(form.is_valid())
