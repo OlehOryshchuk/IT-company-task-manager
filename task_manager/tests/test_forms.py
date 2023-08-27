2fb05e89b38e780f3bb20af013d35fa859d13ad4
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-08-27 22:26:16 +0300

    test: Test TaskCreateForm, test field 'assignees' queryset options are only user team members

--- task_manager/tests/test_forms.py
+++ task_manager/tests/test_forms.py
@@ -13,6 +13,7 @@ from ..form import (
     valid_deadline
 )
 from ..models import TaskType
+from team_manager.models import Team
 
 
 class TaskFormTest(TestCase):
@@ -21,6 +22,7 @@ class TaskFormTest(TestCase):
         self.user = get_user_model().objects.create(
             username="testuser", password="test123"
         )
+        self.task_type = TaskType.objects.create(name="main_task_type")
 
     def test_filter_form_empty_label_and_required_should_be_false(self):
         self.assertEqual(self.filter_form.fields["task_type"].empty_label, "Filter by task type")
@@ -47,13 +49,12 @@ class TaskFormTest(TestCase):
         self.assertIn('placeholder="Search by task name"', rendered_html)
 
     def test_create_form_valid_data(self):
-        task_type = TaskType.objects.create(name="bug")
         form_data = {
             "name": "Test Task",
             "description": "This is a test task",
             "deadline": "2023-08-31",
             "priority": "high",
-            "task_type": task_type.id,
+            "task_type": self.task_type.id,
             "owner": self.user.id,
             "is_completed": True,
         }
@@ -69,3 +70,35 @@ class TaskFormTest(TestCase):
 
         form = TaskCreateForm(data=form_data, user=self.user)
         self.assertFalse(form.is_valid())
+
+    def test_create_form_field_assignees(self):
+        new_team = Team.objects.create(
+            name="NewTeam", owner=self.user
+        )
+        user2 = get_user_model().objects.create(username="user2", password="user2123")
+        user3 = get_user_model().objects.create(username="user3", password="user3123")
+        user4 = get_user_model().objects.create(username="user4", password="user4123")
+
+        new_team.members.add(user2, user3, self.user)
+
+        form_data = {
+            "name": "Test Task",
+            "description": "This is a test task",
+            "deadline": "2023-08-31",
+            "priority": "high",
+            "task_type": self.task_type.id,
+            "owner": self.user.id,
+            "is_completed": True,
+        }
+
+        create_form = TaskCreateForm(user=self.user, data=form_data)
+
+        self.assertTrue(create_form.is_valid())
+        create_form.save()
+
+        # user4 not supposed to be in assignees.queryset because he is not in same team
+        self.assertEqual(
+            list(create_form.fields["assignees"].queryset),
+            [self.user, user2, user3]
+        )
+        self.assertTrue(user4 not in create_form.fields["assignees"].queryset)
