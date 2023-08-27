df3210cbe08fa08226b081fbff894c2e98b6c463
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-08-27 18:59:11 +0300

    test: Test TaskTypeadmin, check if task_type in admin site display it's name

--- task_manager/tests/test_admins.py
+++ task_manager/tests/test_admins.py
@@ -1,9 +1,23 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
-
+from datetime import datetime, timedelta
 from ..models import TaskType, Task, Project
+from team_manager.models import Team
 
 
 class AdminSiteTest(TestCase):
-    pass
+    def setUp(self) -> None:
+        self.admin_user = get_user_model().objects.create_superuser(
+            username="test_username", password="test1234"
+        )
+        self.client.force_login(self.admin_user)
+
+    def test_admin_task_type_list_has_field_name(self):
+        new_task_type = TaskType.objects.create(name="new_task-type")
+        url = reverse("admin:task_manager_tasktype_changelist")
+
+        response = self.client.get(url)
+
+        self.assertContains(response, new_task_type.name)
+
