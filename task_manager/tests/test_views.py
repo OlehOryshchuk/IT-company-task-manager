04faacc81b51f28f4216e496333da2d0967b1e74
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-08-28 17:18:04 +0300

    test: Test unauthorized access to create task type page

--- task_manager/tests/test_views.py
+++ task_manager/tests/test_views.py
@@ -14,3 +14,10 @@ TASK_CREATE = reverse_lazy("task_manager:task-create")
 PROJECT_LIST = reverse_lazy("task_manager:project-list")
 PROJECT_CREATE = reverse_lazy("task_manager:project-create")
 
+
+class PublicTaskTypeTest(TestCase):
+    def test_create_task_type_login_required(self):
+        response = self.client.get(TASK_TYPE_CREATE)
+
+        self.assertNotEqual(response.status_code, 200)
+        self.assertRedirects(response, "/accounts/login/?next=/task/task_type/create/")
