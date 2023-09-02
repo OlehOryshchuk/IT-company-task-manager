39587921b2f59be09437769709143c8fd0c1d7f5
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-09-02 15:35:14 +0300

    refact: Install boostrap5 and set necessary configuration for boostrap5 and set ASSETS_ROOT for templates

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "crispy_forms",
-    "crispy_bootstrap4",
+    "crispy_bootstrap5",
     "taggit_autosuggest",
     "taggit",
     "task_manager",
@@ -79,7 +79,8 @@ TEMPLATES = [
     },
 ]
 
-CRISPY_TEMPLATE_PACK = "bootstrap4"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 WSGI_APPLICATION = "it_company_task_manager.wsgi.application"
 
@@ -136,6 +137,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+ASSETS_ROOT = "/static/assets"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
