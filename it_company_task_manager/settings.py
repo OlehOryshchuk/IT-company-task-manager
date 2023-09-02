dadb6a8e5a3b6bb3454351254b075fee0a85b719
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-09-02 16:02:47 +0300

    feat: Provide to templates 'ASSETS_ROOT' where are all boostrap js & css and other files

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "templates.context_processors.cfg_assets_root",
             ],
             "libraries": {
                 "query_transform": "templatetags.query_transform"
